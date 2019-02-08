8a5bee01e6e98e6f6d036e84316d6263a96c27f2
Sanjay <ashwinisable97@gmail.com>
2019-02-08 10:04:53 +0530

    main actvity deleted

---
+++ app/src/main/java/com/example/poonam/voice_based_elearning/Activities/Faculty_Profile_Activity.java
@@ -0,0 +1,15 @@
+package com.example.poonam.voice_based_elearning.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.poonam.voice_based_elearning.R;
+
+public class Faculty_Profile_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_faculty_profie);
+    }
+}
